21efcab00df8ecfae08a6022e49db6ff0c7b6708
Salim Natha <PAsystemsltd@Salims-iMac-7.local>
2018-01-30 13:50:07 +0000

    Initial commit

---
+++ app/src/main/java/com/example/pasystemsltd/myapp/LessonsList.kt
@@ -0,0 +1,22 @@
+package com.example.pasystemsltd.myapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.GridLayout
+
+class LessonsList : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lessons_list)
+
+
+
+
+
+
+
+
+    }
+}
